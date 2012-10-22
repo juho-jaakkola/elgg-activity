4d3478773e673f01c62ec6ca2ede542ea7a074a4
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-10-22 14:43:40 +0300

    Add missing translation string.

--- languages/fi.php
+++ languages/fi.php
@@ -5,6 +5,8 @@ $lang = array(
 	'activity:module:mentions:title' => 'Uusimmat maininnat',
 	'activity:module:weekly_likes' => 'Viikon tykätyimmät',
 
+	// Automatically generated strings not found from core
+	'groupforumtopic:groupforumtopic' => 'Keskustelu',
 );
 
 add_translation('fi', $lang);
